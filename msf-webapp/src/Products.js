01c3c76bb1ca8a30074f37fc86f0fca580bf670e
Praveen-Mariappasamy <115199875+Praveen-Mariappasamy@users.noreply.github.com>
2024-03-30 17:18:42 +0530

    Update Products.js

--- msf-webapp/src/Products.js
+++ msf-webapp/src/Products.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Products.css';
-export default function(props){
-    let imageSrc;
-    try {
-        imageSrc = require(`./images/${props.value}.webp`).default;
-    } catch (error) {
-        // Handle error, e.g., image not found
-        console.error(`Image for '${props.value}' not found.`);
-    }
-    return(
+
+export default function Product(props) {
+    const [imageSrc, setImageSrc] = useState(null);
+    
+    const loadImage = async () => {
+        try {
+            const imageModule = await import(`./images/${props.value}.webp`);
+            setImageSrc(imageModule.default);
+        } catch (error) {
+            console.error(`Image for '${props.value}' not found.`);
+        }
+    };
+
+    loadImage();
+    console.log(imageSrc)
+    console.log("what")
+    if (!imageSrc) {
+        return (
             <div className="boxes">
-                <img className="photos" src={imageSrc}/>
+                <div>Loading...</div>
                 <h1 className="product-items">{props.value}</h1>
             </div>
-    )
+        );
+    }
+    
+    return (
+        <div className="boxes">
+            <img className="photos" src={imageSrc} alt={props.value} />
+            <h1 className="product-items">{props.value}</h1>
+        </div>
+    );
 }
